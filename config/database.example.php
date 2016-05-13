dc96851edde1870c53293ef4b3fa4341e2c962b4
Kylian LE METTE <kylian.le.mette@gmail.com>
2016-05-13 08:53:22 +0200

    #7 add config file

---
+++ config/database.example.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This file is a sample file. You must copy it informed your database.
+ * You must rename a database.php
+ */
+return [
+    "host" => "",
+    'port' => "",
+    "dbname" => "",
+    "username" => "",
+    "password" => ""
+];
